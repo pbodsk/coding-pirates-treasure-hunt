c11a074f1d7626baac9892434a559b7213e9a10e
Peter Bødskov <pb@reeltrak.com>
2020-01-26 13:14:21 +0100

    new versions, now broadcasing a single letter :)

--- src/receive.py
+++ src/receive.py
@@ -3,8 +3,7 @@ import radio
 radio.on()
 radio.config(power=1)
 while True:
-    incoming = radio.receive()
-    if incoming == "Station1":
-        display.show("s1")    
-    else:
-        display.show("NO") 
+    incoming = str(radio.receive())
+    if incoming == "None":
+        incoming = "-"
+    display.show(incoming) 
--- src/send.py
+++ src/send.py
@@ -1,9 +1,8 @@
 from microbit import *
 import radio
 
-
 radio.on()
 radio.config(power=1)
 while True:
-    radio.send("Station1")
-    display.show("OK")
+    radio.send("P")
+    display.show("P")
